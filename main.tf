ccbbb36191194bef3e57541bd7c6038dbb16f6d3
Cory G Watson <cwatson@signalfx.com>
2020-05-08 07:52:49 -0700

    Adjust to use vars

--- main.tf
+++ main.tf
@@ -1,28 +1,32 @@
+variable "signalfx_key" {
+  type = string
+}
+
 provider "signalfx" {
-	auth_token = "ToKeNgOeShErE"
+	auth_token = var.signalfx_key
 }
 
-resource "signalfx_time_chart" "mychart0" {
-    name = "CPU Total Idle"
-    description = "Very cool chart"
-
-    program_text = <<-EOF
-    data("cpu.total.idle").publish()
-    EOF
-}
-
-resource "signalfx_dashboard_group" "mydashgroup0" {
-  name = "Workshop Group"
-}
-
-resource "signalfx_dashboard" "mydashboard0" {
-  name = "Workshop Dashboard"
-  dashboard_group = signalfx_dashboard_group.mydashgroup0.id
-
-  chart {
-    chart_id = signalfx_time_chart.mychart0.id
-    width = 12
-    height = 1
-    row = 0
-  }
-}
+# resource "signalfx_time_chart" "mychart0" {
+#     name = "CPU Total Idle"
+#     description = "Very cool chart"
+#
+#     program_text = <<-EOF
+#     data("cpu.total.idle").publish()
+#     EOF
+# }
+#
+# resource "signalfx_dashboard_group" "mydashgroup0" {
+#   name = "Workshop Group"
+# }
+#
+# resource "signalfx_dashboard" "mydashboard0" {
+#   name = "Workshop Dashboard"
+#   dashboard_group = signalfx_dashboard_group.mydashgroup0.id
+#
+#   chart {
+#     chart_id = signalfx_time_chart.mychart0.id
+#     width = 12
+#     height = 1
+#     row = 0
+#   }
+# }
